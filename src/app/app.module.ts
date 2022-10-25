1269a9a09106b06aedee3ebd1ec3181e2d061862
Pavlo Hurtovenko <pavlohurtovenko@gmail.com>
2022-10-25 12:42:33 +0300

    created header structure, add material UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './header/auth/auth.component';
+import { HeaderComponent } from './header/header/header.component';
+import { SignUpComponent } from './header/auth/sign-up/sign-up.component';
+import { LogInComponent } from './header/auth/log-in/log-in.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BuilderComponent } from './builder/builder.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    HeaderComponent,
+    SignUpComponent,
+    LogInComponent,
+    BuilderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
